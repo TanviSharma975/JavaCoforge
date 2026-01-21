e5dc213b3954a1ae77b37732737f864f1bc230aa
TanviSharma975 <156917826+TanviSharma975@users.noreply.github.com>
2026-01-21 15:35:31 +0530

    Add files via upload

---
+++ TC__Navigate.java
@@ -0,0 +1,26 @@
+package SeleniumPack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC__Navigate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://tutorialsninja.com/demo/index.php?");
+		driver.navigate().to("https://www.amazon.in/");
+		System.out.println("Title"+driver.getTitle());
+		driver.navigate().back();
+		System.out.println("Title after back:"+driver.getTitle());
+		driver.navigate().forward();
+		System.out.println("Title after forward:"+driver.getTitle());
+		System.out.println(driver.getPageSource());
+		
+
+	}
+
+}
